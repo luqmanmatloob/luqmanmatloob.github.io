59e18d8d43dfee551ef5f8613b50e1dd4970a8de
luqmanmatloob <93182189+luqmanmatloob@users.noreply.github.com>
2023-09-16 16:12:14 +0500

    -

--- portfolio3/js/main.js
+++ portfolio3/js/main.js
@@ -11,3 +11,15 @@ window.addEventListener('load', function() {
         preloader.style.display = 'none';
     }, 500); // 2000 milliseconds = 2 seconds
 });
+
+
+
+// // nav hide 
+// let navBar = document.querySelectorAll('.nav-link');
+// let navCollapse = document.querySelector('.navbar-collapse.collapse');
+// navBar.forEach(function (a) {
+//     a.addEventListener("click", function () {
+//         navCollapse.classList.remove("show");
+//     })
+// })
+
