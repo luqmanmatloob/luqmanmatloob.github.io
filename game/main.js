9dd927914475489356ab088bfa49054ee8aee722
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-03-19 18:16:59 +0500

    js function modification for add/remove the img

--- game/main.js
+++ game/main.js
@@ -1,29 +1,43 @@
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    const guessInput = document.getElementById("guessInput");
-    const guessButton = document.getElementById("guessButton");
-    const message = document.getElementById("message");
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const message = document.getElementById("message");
+const crossElement = document.getElementById("crossBox");
+const tickElement = document.getElementById("tickBox");
 
-    let guessCount = 0;
+let guessCount = 0;
 
-    guessButton.addEventListener("click", checkGuess);
+guessButton.addEventListener("click", checkGuess);
 
-    function checkGuess() {
-        const userGuess = parseInt(guessInput.value);
-        guessCount++;
+// Reset classes
+crossElement.classList.remove("crossBox");
+tickElement.classList.remove("tickBox");
 
-        if (userGuess === randomNumber) {
-            message.textContent = `Congratulations! You guessed the number ${randomNumber} in ${guessCount} attempts.`;
-            message.style.color = "green";
-          
-        } else if (userGuess < randomNumber) {
-            message.textContent = "Try a higher number.";
-            message.style.color = "red";
-        } else {
-            message.textContent = "Try a lower number.";
-            message.style.color = "red";
-        }
+function checkGuess() {
+	const userGuess = parseInt(guessInput.value);
+	guessCount++;
 
-        guessInput.value = "";
-        guessInput.focus();
-    }
+	if (userGuess === randomNumber) {
+		message.textContent = `Congratulations! You guessed the number ${randomNumber} in ${guessCount} attempts.`;
+		message.style.color = "green";
+		tickElement.classList.add("tickBox");
+		crossElement.classList.remove("crossBox");
+	} else if (userGuess < randomNumber) {
+		message.textContent = "Try a higher number.";
+		message.style.color = "red";
+		tickElement.classList.remove("tickBox");
+		crossElement.classList.add("crossBox");
+	} else if (userGuess > randomNumber) {
+		message.textContent = "Try a Lower number.";
+		message.style.color = "red";
+		tickElement.classList.remove("tickBox");
+		crossElement.classList.add("crossBox");
+	} else {
+		message.textContent = "Game Crashed"; // Unknown state: exception handling for this later
+		message.style.color = "red";
+	}
+
+	guessInput.value = "";
+	guessInput.focus();
+}
